3ba334f8fa84193b0f2ab09b91e91c3f1280591b
Harsh Shrivastava <harsh.shrivastava.eee21@itbhu.ac.in>
2024-09-29 03:52:10 +0530

    added cors credentials

--- server/index.js
+++ server/index.js
@@ -8,7 +8,10 @@ const { ServerConfig, ConnectDB } = require("./config");
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: `${ServerConfig.FRONTEND_URL}`,
+  credentials: true, 
+}));
 
 app.use(DbConnectionMiddleware.dbConnectionMiddleware);
 
