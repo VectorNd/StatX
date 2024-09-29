918b6602e0a2dee292954a3fd48099c61f723f03
Harsh Shrivastava <harsh.shrivastava.eee21@itbhu.ac.in>
2024-09-29 05:57:59 +0530

    replaced cookie with url param

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,13 @@
 import { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "./context/AuthContext";
-import Cookies from "js-cookie";
 
 function App() {
   const navigate = useNavigate();
   const { setJwt, jwt } = useContext(AuthContext);
 
   useEffect(() => {
-    const jwtVal = Cookies.get("jwt");
-    if (jwtVal) {
-      setJwt(jwtVal);
-    } else {
+    if (!jwt) {
       navigate('/login');
     }
   }, []);
