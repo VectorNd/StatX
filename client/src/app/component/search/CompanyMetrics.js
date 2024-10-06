24b40f9da5bbe75b685036a52f766abec5c83ac6
Harsh Shrivastava <harsh.shrivastava.eee21@itbhu.ac.in>
2024-10-06 05:47:26 +0530

    added prediction

--- client/src/app/component/search/CompanyMetrics.js
+++ client/src/app/component/search/CompanyMetrics.js
@@ -3,24 +3,39 @@ import "../../styles/styles.css";
 import Graph from "../content/Graph";
 import SameCountry from "../content/SameCountry";
 import UnionOne from "../content/UnionOne";
+import Prediction from "../content/Prediction";
 
 const CompanyMetrics = ({ data }) => {
-
   return (
     <>
-    <div className="metrics-container">
-      <div className="metrics-left-div">
-        <div className="same-country">
-          <SameCountry data={data.totalCompaniesInCountry} type="same-country"/>
-          <SameCountry data={data.greaterDiversity} type="diversity"/>
+      <div className="metrics-main-container">
+        <div className="metrics-container">
+          <div className="metrics-left-div">
+            <div className="same-country">
+              <SameCountry
+                data={data.totalCompaniesInCountry}
+                type="same-country"
+              />
+              <SameCountry data={data.greaterDiversity} type="diversity" />
+            </div>
+            <Graph data={data.yearlyChanges} />
+          </div>
+          <div className="metrics-right-div">
+            <UnionOne data={data} />
+          </div>
         </div>
-        <Graph data={data.yearlyChanges}/>
+        <div style={{fontSize: "23px", width: "200px", fontWeight: "bold", marginLeft: "20px"}}>Prediction:</div>
+        <div className="metrics-container" style={{ flexDirection: "column" }}>
+          <div className="same-prediction">
+            <Prediction data={data.totalCompaniesInCountry} type="stock" />
+            <Prediction data={data.greaterDiversity} type="market" />
+          </div>
+          <div className="same-prediction">
+            <Prediction data={data.totalCompaniesInCountry} type="revenue" />
+            <Prediction data={data.greaterDiversity} type="expense" />
+          </div>
+        </div>
       </div>
-      <div className="metrics-right-div">
-        <UnionOne data={data}/>
-      </div>
-    </div>
-     
     </>
   );
 };
